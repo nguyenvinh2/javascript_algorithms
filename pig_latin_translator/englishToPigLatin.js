c609350245c15aa50bdfd1ff357f09b340a1d5d4
nguye <nguyenv2@outlook.com>
2019-03-15 08:55:13 -0700

    first challenge

---
+++ pig_latin_translator/englishToPigLatin.js
@@ -0,0 +1,20 @@
+'use strict';
+
+function pigLatin(word) {
+    let vowels = ['a','e','i','o','u'];
+    let result = [];
+    let sliceWord = [];
+    let endWord = 'ay';
+    for(let i=0; i<word.length; i++) {
+        if(!vowels.includes(word[i])) {
+            result.push(word[i]);
+        }
+        else {
+            sliceWord = word.slice(i);
+            break;
+        }
+    }
+    return sliceWord.concat('',result.join('')).concat('',endWord);
+}
+
+console.log(pigLatin('lovelace'));
