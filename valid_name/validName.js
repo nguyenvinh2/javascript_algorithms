6d79c0d7ed7bc1cbc9b4ef5c080a359fffd1d13a
Vinh Nguyen <vinhngy@amazon.com>
2019-03-25 11:37:09 -0700

    this may be working

--- valid_name/validName.js
+++ valid_name/validName.js
@@ -12,12 +12,18 @@ function validNameCheck(inputName) {
       console.log('fails initial check')
       return false;
     }
+    if (inputArray[0].length < 3 && inputArray[0].includes('.')) {
+      if (i !== 0 && i !== inputArray.length - 1 && (inputArray[i].length > 2 || !inputArray[i].includes('.')) ) {
+        console.log('what does this fail?' + ' ' + i + ' ' +inputArray[i].length);
+        return false;
+      }
+    }
     if (inputArray[i][0] !== inputArray[i][0].toUpperCase()) {
       console.log('fails capital letter check');
       return false;
     }
-    if (i === inputArray.length) {
-      if (inputArray[i].includes(".") || inputArray) {
+    if (i === inputArray.length - 1) {
+      if (inputArray[i].includes(".") || inputArray[i].length<2) {
         console.log('last word must not be an initial');
         return false;
       }
@@ -26,4 +32,4 @@ function validNameCheck(inputName) {
   return true;
 }
 
-console.log(validNameCheck('H. Go Wells'));
+console.log(validNameCheck('H. Gree Wells'));
