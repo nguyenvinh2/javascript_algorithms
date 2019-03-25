f35cd880f887f58740b048244551db2b5e9513d0
Vinh Nguyen <vinhngy@amazon.com>
2019-03-25 10:57:05 -0700

    close to completion

--- valid_name/validName.js
+++ valid_name/validName.js
@@ -1,2 +1,29 @@
 'use strict';
 
+function validNameCheck(inputName) {
+  let inputArray = inputName.split(" ");
+  console.log(inputArray);
+  if (inputArray.length <2) {
+    console.log('fails word length check');
+    return false;
+  }
+  for (let i = 0; i < inputArray.length; i++) {
+    if (inputArray[i].includes(".") && inputArray[i].length > 2) {
+      console.log('fails initial check')
+      return false;
+    }
+    if (inputArray[i][0] !== inputArray[i][0].toUpperCase()) {
+      console.log('fails capital letter check');
+      return false;
+    }
+    if (i === inputArray.length) {
+      if (inputArray[i].includes(".") || inputArray) {
+        console.log('last word must not be an initial');
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+console.log(validNameCheck('H. Go Wells'));
