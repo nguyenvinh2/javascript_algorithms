afa0c8c6f7a88be131d6c0a7bc7afa8979ce7a70
Vinh Nguyen <nguyenv2@outlook.com>
2019-03-25 12:07:04 -0700

    eslint spacing

--- pig_latin_translator/englishToPigLatin.js
+++ pig_latin_translator/englishToPigLatin.js
@@ -1,19 +1,19 @@
 'use strict';
 
 function pigLatin(word) {
-    let vowels = ['a','e','i','o','u'];
-    let result = [];
-    let sliceWord = [];
-    let endWord = 'ay';
-    for(let i=0; i<word.length; i++) {
-        if(!vowels.includes(word[i])) {
-            result.push(word[i]);
-        }
-        else {
-            sliceWord = word.slice(i);
-            break;
-        }
+  let vowels = ['a', 'e', 'i', 'o', 'u'];
+  let result = [];
+  let sliceWord = [];
+  let endWord = 'ay';
+  for (let i = 0; i < word.length; i++) {
+    if (!vowels.includes(word[i])) {
+      result.push(word[i]);
     }
-    return sliceWord.concat('',result.join('')).concat('',endWord);
+    else {
+      sliceWord = word.slice(i);
+      break;
+    }
+  }
+  return sliceWord.concat('', result.join('')).concat('', endWord);
 }
 console.log(pigLatin('lovelace'));
